bfe03ba94b479c41c36c4524e6ed0b2357afdac6
Amiio974 <matthieu.debaisieux.pro@gmail.com>
2026-09-02 10:33:19 +0200

    Modification de la width et autre
    Création page de Stage
    
    Modificatin de la navbar
    
    modification de la width des mains

--- src/main.js
+++ src/main.js
@@ -1,18 +1,1 @@
 import '../Styles/style.css'
-import { createTimeline, stagger, splitText, animate, svg  } from 'animejs';
-
-const { chars } = splitText('#hero', {
-    chars: {
-        wrap: 'clip',
-        clone: 'bottom'
-    },
-});
-
-createTimeline()
-.add(chars, {
-    y: '-115%',
-    loop: true,
-    loopDelay: 9500,
-    duration: 750,
-    ease: 'inOut(2)',
-}, stagger(150, { from: 'center' }));
